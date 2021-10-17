e62aeeefa773baece1235b61dfc41e7dd992ae68
Mikey Lau <mikey.lau@traininblocks.com>
2021-10-17 16:25:15 +0100

    Working commands

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 require("dotenv").config();
-const { Client } = require("discord.js");
-const client = new Client();
+const { Client, Intents } = require("discord.js");
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once("ready", () => {
+  console.log("Ready!");
 });
 
-client.on("message", (msg) => {
-  if (msg.content === "ping") {
-    msg.reply("Pong!");
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === "ping") {
+    await interaction.reply("Pong!");
+  } else if (commandName === "server") {
+    await interaction.reply("Server info.");
+  } else if (commandName === "user") {
+    await interaction.reply("User info.");
   }
 });
 
